40d79e30fc289d7b3f973bafa4157fad1e826b81
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-08-02 15:48:53 +0700

    Cài đặt các thư viện, cấu hình routes, config,...

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,14 @@
+import config from '~/config';
 
+import Home from '~/pages/Home';
+
+const publicRoutes = [
+  {
+    path: config.routes.home,
+    component: Home,
+  },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
